16ea6e251e2a8ee301d17814e468ca5c1f5d84a1
Taichi Yasui <yasutai12@gmail.com>
2024-06-20 14:33:39 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
+    // setNum(num + 1);
+  };
   const contentStyleB = {
     color: "green",
     fontSize: "18px",
@@ -11,7 +17,8 @@ export const App = () => {
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="green">元気です!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
